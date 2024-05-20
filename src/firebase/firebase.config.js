a8bd88f0a653a3a45194c403c05a71d2097e24bc
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-05-20 07:41:26 +0530

    config changes

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,11 +13,11 @@ const firebaseConfig = {
   storageBucket: "ecommerceadvanced-b7838.appspot.com",
   messagingSenderId: "1045249808274",
   appId: "1:1045249808274:web:128f2a8a9f9ea1bcced34b",
-//   measurementId: "G-PLVLT8FJZN"
+  measurementId: "G-PLVLT8FJZN"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 
 export default app;
